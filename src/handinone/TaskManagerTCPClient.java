b64c86e4e97ca6ddfc666f825aabdf79bd3cb612
Claus L. Henriksen <clih@itu.dk>
2012-09-10 16:56:18 +0200

    Client v. 0.1 done. GetInputStream() works?

--- src/handinone/TaskManagerTCPClient.java
+++ src/handinone/TaskManagerTCPClient.java
@@ -1,7 +1,9 @@
 package handinone;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -33,6 +35,22 @@ public class TaskManagerTCPClient {
 			// Get data output stream to send a String message to server.
 			DataOutputStream dos = new DataOutputStream(
 					socket.getOutputStream());
+
+			// Get the inputstream to receive data sent by server.
+			InputStream is = socket.getInputStream();
+
+			// based on the type of data we want to read, we will open suitbale
+			// input stream.
+			DataInputStream dis = new DataInputStream(is);
+
+			// Send message
+			String message = "Ping";
+			dos.writeUTF(message);
+			dos.flush();
+			
+			// Receive responce and print
+			String responce = dis.readUTF();
+			System.out.println("Message from server: " + responce);
 
 			// Finnaly close the socket.
 			socket.close();
