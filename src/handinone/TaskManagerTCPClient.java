4dad59f455beca9fa84a682d3a1dd2eba35db353
Claus L. Henriksen <clih@itu.dk>
2012-09-10 16:40:14 +0200

    empty files

---
+++ src/handinone/TaskManagerTCPClient.java
@@ -0,0 +1,13 @@
+package handinone;
+
+public class TaskManagerTCPClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/handinone/TaskManagerTCPServer.java
@@ -0,0 +1,13 @@
+package handinone;
+
+public class TaskManagerTCPServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
