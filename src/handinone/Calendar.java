b4d09d2ab08f68283a3c557961ff9fffc4d507c2
Michael Soeby Andersen <michael@mileratec.com>
2012-09-11 11:05:45 +0200

    NO WARNING MOFO :D

--- src/handinone/Calendar.java
+++ src/handinone/Calendar.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
